e0c0f9c64085894146177a4876da06740d145b52
emintzcn <m.emintzcn@gmail.com>
2023-01-06 22:55:30 +0300

    6 Ocak 2023 ders sonu

---
+++ Döngüler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Döngüler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
---
+++ Kampİntro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Kampİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool sistemeGirisYapmısMı = false;
+
+
+            if (sistemeGirisYapmısMı == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+        }
+    }
+}
